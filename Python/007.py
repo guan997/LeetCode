e0084d59e5082679812988104bd1ff3cbabe86b9
ggz_66@163.com <ggz_66@163.com>
2020-03-30 23:57:51 +0800

    007

--- Python/007.py
+++ Python/007.py
@@ -1,17 +1,11 @@
 #7给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
-def ssreverse(x):
-     if (-2^31)<= x <=(2^31-1):
-          return x
-     else:
-          return 0
+def reverse(x):
      str_x=str(x)
-     if str_x[0]!='-':
-          str_x=str_x[::-1]
-          x=int(str_x)
-     else:
-          str_x=str_x[:0:-1]
-          x=int(str_x)
-          x=-x
-     return x if (-2^31)<= x <=(2^31-1) else 0
-          
-       
+        if str_x[0]!='-':
+            str_x=str_x[::-1]
+            x=int(str_x)
+        else:
+            str_x=str_x[:0:-1]
+            x=int(str_x)
+            x=-x
+        return x if (-2**31)<= x <=(2**31-1) else 0
