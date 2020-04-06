257a379ebc66d581227403624f8853f0b1d4ad88
ggz_66@163.com <ggz_66@163.com>
2020-04-06 23:59:48 +0800

    237删除链表中的节点

---
+++ Python/237.py
@@ -0,0 +1,10 @@
+#237. 删除链表中的节点
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        
